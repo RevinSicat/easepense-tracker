81c998051419eb5a21b733649365a76c93cc4d75
Revin Sicat <SicatRevin@outlook.com>
2025-09-22 10:36:11 +0800

    Added Id generation in Account Model

--- src/main/java/com/ease/easepensetracker/model/AccountModel.java
+++ src/main/java/com/ease/easepensetracker/model/AccountModel.java
@@ -1,5 +1,8 @@
 package com.ease.easepensetracker.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
@@ -9,6 +12,12 @@ import lombok.Data;
 public class AccountModel {
     @Id
     String id;
+    @Column(nullable = false)
     String name;
+    @Column(nullable = false)
     private double balance;
+
+    public AccountModel() {
+        this.id = UUID.randomUUID().toString().split("-")[0];
+    }
 }
