9d3239c58320c9ad22cd2aef4a83fe08a38e8c4c
Revin Sicat <SicatRevin@outlook.com>
2025-09-22 11:03:36 +0800

    Init Transaction Model

---
+++ src/main/java/com/ease/easepensetracker/model/Transaction.java
@@ -0,0 +1,47 @@
+package com.ease.easepensetracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Transaction {
+    @Id
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private CategoryModel category;
+
+    @Column(nullable = false) 
+    private String title; //default to Category name if not use format <categoryname>: <title>
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
+    private Date date = new Date();
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false) 
+    private String type; // default to category type
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private AccountModel account;
+
+    private String notes; // can be null
+
+    public Transaction() {
+        this.id = UUID.randomUUID().toString();
+    }
+}
