562897cab0e7ecb7d4dba81b43755ed4f2b1fa99
Revin Sicat <SicatRevin@outlook.com>
2025-09-22 10:01:20 +0800

    Init Project

---
+++ src/main/java/com/ease/easepensetracker/EasepenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ease.easepensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasepenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasepenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ease/easepensetracker/EasepenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ease.easepensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasepenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
